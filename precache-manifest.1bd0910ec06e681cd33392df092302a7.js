f686925390551e05d35c953718151e03e03a82a9
andreamalvetta <andrea.malvetta@gmail.com>
2019-08-28 14:18:40 +1000

    Updates

---
+++ precache-manifest.1bd0910ec06e681cd33392df092302a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778bee174e8264d81943cd124cee5654",
+    "url": "/ReactTicTacToe/index.html"
+  },
+  {
+    "revision": "8ade0b3fb915d2d145a4",
+    "url": "/ReactTicTacToe/static/css/main.03d6ba47.chunk.css"
+  },
+  {
+    "revision": "567b947e267a8c1be957",
+    "url": "/ReactTicTacToe/static/js/2.4db794a0.chunk.js"
+  },
+  {
+    "revision": "8ade0b3fb915d2d145a4",
+    "url": "/ReactTicTacToe/static/js/main.89fb96d3.chunk.js"
+  },
+  {
+    "revision": "ef29e91424d5de5e1aae",
+    "url": "/ReactTicTacToe/static/js/runtime~main.e9d75eba.js"
+  }
+]);
